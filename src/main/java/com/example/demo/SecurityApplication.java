57f69f34b8a83e1bf82fe307b99edb245de25606
saif alam <saif.alam@peopletech.com>
2024-12-04 22:37:00 +0530

    implemented rbac auth in spring boot

---
+++ src/main/java/com/example/demo/SecurityApplication.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+
+
+import com.example.demo.auth.AuthenticationService;
+import com.example.demo.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static com.example.demo.user.Role.ADMIN;
+import static com.example.demo.user.Role.MANAGER;
+
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+}
